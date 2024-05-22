6b91bfbb633d0787165ece7cc798d35d5e1a8cbb
LeaPar4 <lealea21315@gmail.com>
2024-05-22 16:14:37 -0700

    set everything up i think

---
+++ mc fabric mod template/src/main/java/modtest/modid/ModTestClient.java
@@ -0,0 +1,10 @@
+package modtest.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModTestClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
